59a3e0c8633083e02dc5d4bcae6f9bea4c37d647
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-08-22 21:31:10 +0600

    third

--- js/player.js
+++ js/player.js
@@ -1,4 +1,20 @@
+function getButtonValueById(btnCLick){
+  document.getElementById('color').style.backgroundColor='bg-green-200';
+  
 
+  const playerList = document.getElementById('order-list');
+  const newPlayer = document.createElement('li');
+  const player = btnCLick;
+  if(playerList.children.length < 5){
+    newPlayer.innerText = player;
+    playerList.appendChild(newPlayer);
+
+    
+  }
+  else{
+    alert('You can not choose more than 5 players');
+  }
+}
 
 
 
