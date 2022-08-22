0de4c274b21c68bf272600571da03c1d69fad7cf
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-08-22 22:04:10 +0600

    sixth

--- js/functionality.js
+++ js/functionality.js
@@ -1,1 +1,13 @@
+function getInputValueById(inpuId){
+  const inputField = document.getElementById(inpuId);
+  const inputValueString = inputField.value;
+  const inputValue = parseInt(inputValueString);
+  return inputValue;
+}
 
+function getTextValueById(elementId){
+  const textElement = document.getElementById(elementId);
+  const textValueString =textElement.innerText;
+  const textValue = parseInt(textValueString);
+  return textValue;
+}
