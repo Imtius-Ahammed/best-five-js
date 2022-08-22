7c743c19cd986336feb7b89f400eceb92555c096
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-08-22 22:49:23 +0600

    7th

--- js/player.js
+++ js/player.js
@@ -1,41 +1,17 @@
 function getButtonValueById(btnCLick){
-
-  
-
-  const button1 = document.getElementById('color-1');
-  button1.style.backgroundColor='gray';
-
-  const button2 = document.getElementById('color-2');
-  button2.style.backgroundColor='gray';
-
-  const button3 = document.getElementById('color-3');
-  button3.style.backgroundColor='gray';
-
-  const button4 = document.getElementById('color-4');
-  button4.style.backgroundColor='gray';
-
-  const button5 = document.getElementById('color-5');
-  button5.style.backgroundColor='gray';
-  
-  const button6 = document.getElementById('color-6');
-  button6.style.backgroundColor='gray';
- 
-  
-
-  
-
   const playerList = document.getElementById('order-list');
   const newPlayer = document.createElement('li');
   const player = btnCLick;
   if(playerList.children.length < 5){
     newPlayer.innerText = player;
     playerList.appendChild(newPlayer);
-
-    
+    this.disabled=true;
   }
   else{
     alert('You can not choose more than 5 players');
   }
+
+  
 }
 
 
@@ -49,6 +25,22 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
 
   const playerExpenses = document.getElementById('player-expenses');
   playerExpenses.innerText = calculateResult;
+})
+
+// manager and coach
+
+document.getElementById('calculate-total').addEventListener('click', function(){
+  const managerInput = getInputValueById('manager-input');
+
+ 
+  const coachInput = getInputValueById('coach-input');
+
+  const playerExpenses = getTextValueById('player-expenses');
+
+  const calculateTotal = managerInput + coachInput + playerExpenses;
+
+  const total = document.getElementById('total');
+  total.innerText = calculateTotal;
 })
 
 
